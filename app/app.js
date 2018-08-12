0f614b460d200a9deb086aa501fe09a3790270bb
villalbaweb <villalbaweb@gmail.com>
2018-08-12 08:51:30 -0500

    Refactoring app.js for readhability

--- app/app.js
+++ app/app.js
@@ -6,21 +6,19 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Please enter a XML string...: ', (answer) => {
-        
-    //answer = '<payment><amount>10.00</amount><from>Evan</from><to>PayStand</to></payment>';    // just to development
-    answer = '<shipment><item>some item</item><from>Evan</from><to>PayStand</to><address><item>some item</item><street>100 Enterprise Way</street><city>Scotts Valley</city><zip>95066</zip></address></shipment>';
-
-    console.log(`XML string length: ${answer.length}`);
-    console.log('----------------------------------------------------------');
-
-    var startingMsg = answer.length > 0 ? 'processing...' : 'No XML string received...';
-    console.log(startingMsg);
-
-    if(answer.length > 0) {
-        var result = customParser.processXML(answer);
-        console.log(`\nParsed JSON object:\n ${result}\n`);
+rl.question('\nPlease enter a XML string or ENTER to use default XML ...: ', (answer) => {
+    
+    if( answer.length === 0) {
+        answer = '<shipment><item>some item</item><from>Evan</from><to>PayStand</to><address><item>some item</item><street>100 Enterprise Way</street><city>Scotts Valley</city><zip>95066</zip></address></shipment>';
     }
+
+    console.log(`\nXML input:\n${answer}\n`);
+    console.log(`XML string length: ${answer.length}`);
+    console.log('processing...');
+
+    var result = customParser.processXML(answer);
+
+    console.log(`\nParsed JSON object:\n ${result}\n`);
 
     rl.close();
 });
